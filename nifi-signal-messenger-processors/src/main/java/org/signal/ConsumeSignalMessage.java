eec1130897f5c418b9a8bf6f76770cd62d948f92
Christoffer Lind <christoffer.lind@yandex.com>
2020-04-29 22:43:30 +0200

    Added support for sending reaction

--- nifi-signal-messenger-processors/src/main/java/org/signal/ConsumeSignalMessage.java
+++ nifi-signal-messenger-processors/src/main/java/org/signal/ConsumeSignalMessage.java
@@ -180,13 +180,12 @@ public class ConsumeSignalMessage extends AbstractSessionFactoryProcessor {
 		Map<String, String> attributes = new HashMap<>(7);
 		try {
 			attributes.put(ATTRIBUTE_SENDER_IDENTIFIED, Boolean.toString(!envelope.isUnidentifiedSender()));
-			attributes.put(ATTRIBUTE_SENDER_NUMBER, 	envelope.getSourceAddress().getNumber().get());
-			attributes.put(ATTRIBUTE_TIMESTAMP, 		Long.toString(envelope.getTimestamp()));
 			attributes.put(ATTRIBUTE_RECEIPT, 			Boolean.toString(Boolean.FALSE));
 
 			SignalServiceContent decryptedMessage = service.decryptMessage(envelope);
 			String senderNumber = decryptedMessage.getSender().getNumber().get();
-			attributes.put(ATTRIBUTE_SENDER_NUMBER, senderNumber);
+			attributes.put(ATTRIBUTE_SENDER_NUMBER, 	senderNumber);
+			attributes.put(ATTRIBUTE_TIMESTAMP, 		Long.toString(decryptedMessage.getTimestamp()));
 
 			Optional<SignalServiceReceiptMessage> receiptMessage = decryptedMessage.getReceiptMessage();
 			if(envelope.isReceipt() || receiptMessage.isPresent()) {
